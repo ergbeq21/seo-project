920015aa8f74dd4a04f910fe6a58ae7295758348
ergitbeqiri <ErgBeq21@htl-shkoder.com>
2026-03-04 12:28:05 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import background from '$lib/assets/device.jpg'
+</script>
+
+<header class="h-125 bg-center bg-cover flex items-center justify-center" style="background-image:url({background})">
+    <h1 class="text-5xl">Electronic Devices</h1>
+</header>
+
+<main>
+
+</main>
+
+
+<footer>
+
+</footer>
