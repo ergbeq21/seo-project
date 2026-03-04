3c45e0a72d1c02c891f2583d56581bbea8113bae
ergitbeqiri <ErgBeq21@htl-shkoder.com>
2026-03-04 12:41:13 +0100

    added title and description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,12 @@
     import background from '$lib/assets/device.jpg'
 </script>
 
+
+<svelte:head>
+    <title>Ergit's Webshop - Electronics for all!</title>
+    <meta name="description" content="We sell used electronics for a super cheap price!"/>
+</svelte:head>
+
 <header class="h-125 bg-center bg-cover flex items-center justify-center" style="background-image:url({background})">
     <h1 class="text-5xl">Electronic Devices</h1>
 </header>
